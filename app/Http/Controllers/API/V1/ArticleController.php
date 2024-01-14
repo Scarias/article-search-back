f4f89416b4263a9dcc811db533ad730309d3f86e
Bastian Soto Iturra <64429095+Scarias@users.noreply.github.com>
2024-01-14 13:06:17 -0300

    one-to-many user-article relationship added

--- app/Http/Controllers/API/V1/ArticleController.php
+++ app/Http/Controllers/API/V1/ArticleController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\API\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
+use Illuminate\Database\Eloquent\Collection;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : Collection
     {
-        return Article::all();
+        return Article::with('user:id,name')
+            ->get();
     }
 
     /**
@@ -27,9 +29,11 @@ class ArticleController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id) : Collection
     {
-        //
+        return Article::with('user:id,name')
+            ->where('id', $id)
+            ->get();
     }
 
     /**
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
@@ -13,4 +14,8 @@ class Article extends Model
         'title',
         'content',
     ];
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
