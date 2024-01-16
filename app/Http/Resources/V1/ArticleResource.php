26b6a84c0ca3bf275c1a73dee9499237c10fbecd
Bastian Soto Iturra <64429095+Scarias@users.noreply.github.com>
2024-01-15 21:41:28 -0300

    User not-sensible information added in response

--- app/Http/Resources/V1/ArticleResource.php
+++ app/Http/Resources/V1/ArticleResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,11 +16,15 @@ class ArticleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $includeUsers = $request->query('includeUsers');
+
         return [
             'id' => $this->id,
             'title' => $this->title,
             'content' => $this->content,
-            'createdBy' => $this->user_id,
+            'createdBy' => $includeUsers
+                ? User::find($this->user_id, ['id', 'name'])
+                : $this->user_id,
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];
